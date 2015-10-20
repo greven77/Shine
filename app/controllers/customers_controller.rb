2f80e297caa814328e4c137ecf349643066754d3
Antonio Pacheco <greven77@gmail.com>
2015-10-20 15:06:51 +0000

    Finished porting customer search to angular

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,6 +13,10 @@ class CustomersController < ApplicationController
   	else
   		@customers = []
   	end
+    respond_to do|format|
+      format.html {}
+      format.json { render json: @customers}
+    end
   end
 end
 # ctrl + alt + enter -> activate emmet
